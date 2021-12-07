f15d17a45c33e4381ab35fc467eb755bbc454525
Dplo1514 <dladlsgur3334@gmail.com>
2021-12-07 22:23:21 +0900

    212107 readyForHanghae

---
+++ pythonprac/dbquiz.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+#매트릭스의 평점 가져오기
+matrix = db.movies.find_one({'title':'매트릭스'})
+print(matrix['point'])
+
+#매트릭스와 같은 평점의 영화 제목 가져오기
+same_point = list(db.movies.find({'point':'9.39'},{'_id':False}))
+for same_points in same_point :
+    print(same_points['title'])
+
+#매트릭스 영화 평점을 0으로 만들기
+db.movies.update_one({'title':'매트릭스'},{'$set':{'point':'0'}})
+
+
