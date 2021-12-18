e3b92d82aba3c2bc9bf361b1408cc839aa924ded
Dplo1514 <dladlsgur3334@gmail.com>
2021-12-18 22:55:39 +0900

    121218 readyForHanghae

--- homework/homework_4/app.py
+++ homework/homework_4/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, jsonify, request
-from bs4 import BeautifulSoup
-import requests
+from flask import Flask, render_template, jsonify , request
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+#client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 ## HTML 화면 보여주기
@@ -46,4 +45,4 @@ def view_orders():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
--- project/moviestar/app.py
+++ project/moviestar/app.py
@@ -23,16 +23,23 @@ def show_stars():
 
 @app.route('/api/like', methods=['POST'])
 def like_star():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'like 연결되었습니다!'})
+    name_receive = request.form['name_give']
+
+    target_star = db.mystar.find_one({'name': name_receive})
+    current_like = target_star['like']
+    new_like = current_like + 1
+
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like }})
+
+    return jsonify({'msg': 'like + 1 !'})
 
 
 @app.route('/api/delete', methods=['POST'])
 def delete_star():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'delete 연결되었습니다!'})
+    name_receive = request.form['name_give']
+    db.mystar.delete_one({'name': name_receive})
+
+    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':
